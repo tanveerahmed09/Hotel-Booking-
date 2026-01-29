07427dc78c3f7a2239a1f0754087b77e6f710e57
Tanveer Ahmed <tanveeransari0947@gmail.com>
2026-01-29 12:13:43 +0530

    Initial frontend complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
+
 
 const App = () => {
 
@@ -12,11 +19,19 @@ const App = () => {
   return (
 <div className="min-h-screen flex flex-col">  
      {!isOwnerPath && <Navbar/> } 
+    { false && <HotelReg/>}
      <div className='flex-1'>
         <Routes>
              <Route path='/' element= {<Home/>} />             
-             <Route path='/rooms' element= {<AllRooms/>} />             
+             <Route path='/rooms' element= {<AllRooms/>} />
              <Route path='/rooms/:id' element= {<RoomDetails/>} />             
+             <Route path='/my-bookings' element= {<MyBookings/>} />  
+             <Route path='/owner' element={<Layout/>}>
+                 <Route index element={<Dashboard/>}/>
+                 <Route path='add-room' element={<AddRoom/>}/>
+                 <Route path='list/room' element={<ListRoom/>}/>
+                
+            </Route>           
         </Routes>
      </div>
         <Footer/>
--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, facilityIcons, roomsDummyData } from '../assets/assets'
+import { assets, facilityIcons, roomCommonData, roomsDummyData } from '../assets/assets'
 import StarRating from '../components/StarRating'
 
 const RoomDetails = () => {
@@ -73,7 +73,83 @@ const RoomDetails = () => {
            </div>
              
              {/* CheckIn CheckOut Fome */}
+            <form className='flex flex-col md:flex-row items-start md:items-center justify-between
+              bg-white shadow-[0px_0px_2px_rgba(0,0,0,0.15)] p-6 rounded-xl  max-auto mt-16 max-w-6xl'>
 
+               <div className='flex flex-col flex-wrap md:flex-row items-start md:items-center
+               gap-4 md:gap-10 text-gray-500'>
+
+                   <div className='flex flex-col'>
+                       <label htmlFor="checkInDate" className='font-medium'>Check-In</label>
+                       <input type="date" id='checkInDate' placeholder='Check-In ' className='
+                       w-full rounded border border-gray-500 px-3 py-2 mt-1.5 outline-none required' />
+                   </div>
+
+                    <div className='w-px h-15 bg-gray-300/70 max:md-medium'></div>
+
+                   <div className='flex flex-col'>
+                       <label htmlFor="checkOutDate" className='font-medium'>Check-Out</label>
+                       <input type="date" id='checkOutDate' placeholder='Check-Out ' className='
+                       w-full rounded border border-gray-500 px-3 py-2 mt-1.5 outline-none required' />
+                   </div>
+
+                   <div className='w-px h-15 bg-gray-300/70 max:md-medium'></div>
+
+                   <div className='flex flex-col'>
+                       <label htmlFor="guests" className='font-medium'>Guests</label>
+                       <input type="number" id='guests' placeholder='0' className='
+                       max-w-20 rounded border border-gray-300 px-3 py-2 mt-1.5 outline-none required' />
+                   </div>
+
+               </div>
+
+               <button type='sumbit' className='bg-primary hover:bg-primary-dull active:scale-95 
+               transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 
+               text-base cursor-pointer'>
+                 Check Availability
+               </button>
+
+            </form>
+            {/* Common Specifications */}
+                <div className='mt-25 space-y-4'>
+                  {roomCommonData.map((spec, index)=>(
+                    <div key={index} className='flex items-center gap-2'>
+                      <img src={spec.icon} alt={`${spec.title}-icon`} className='w-6.5' />
+                      <div>
+                          <p className='text-base'>{spec.title}</p>
+                          <p className='text-gray-500'>{spec.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div className='max-w-3xl border-y border-gray-300 my-15 py-10 text-gray-500'>
+                 <p>
+                  Guests Will be allocated on the ground floor according to avilability.
+                  You get a comfortable Two bedroom apartment has a ture city feeling. The 
+                  price quoted is for two guests, at the guest slot please mark the number of 
+                  guests to get the exact price for groups. The Guests will be allocated 
+                  ground floor according to avilability. You get the comfortable two bedroom apartment 
+                  that has a true city feeling.   
+                 </p>
+                </div>
+                {/* Hosted by */}
+                <div className='flex flex-col items-start gap-4'>
+                   <div className='flex gap-4'>
+                    <img src={room.hotel.owner.image} alt="Host" className='h-14 w-14 
+                    md:h-18 md:w-18 rounded-full' />
+                      <div>
+                        <p className='text-lg md:text-xl'> Hosted by {room.hotel.name}</p>
+                         <div className='flex items-center mt-1'>
+                          <StarRating/>
+                         <p className='ml-2'>200+ reviwes</p>
+                         </div>
+                      </div>
+                   </div>
+                   <button className='px-6 py-2.5 mt-4 rounded text-white bg-primary
+                   hover:bg-primary-dull transition-all cursor-pointer'
+                   >Contact Now</button>
+                </div>
     </div>
   )
 }
